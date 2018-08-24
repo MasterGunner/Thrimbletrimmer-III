80ca6457c395f83c83ce537e350da58d4234b858
MasterGunner <jimbob959@gmail.com>
2018-08-24 00:44:37 -0300

    Refactoring code

--- src/scripts/Thrimbletrimmer.js
+++ src/scripts/Thrimbletrimmer.js
@@ -14,182 +14,58 @@ var wubs = {
     extraMetadata:function(){return [];}
 }
 
+var Thrimbletrimmer = {};
+
 var PageSetup = function() {
     var videoPlayer = document.getElementById("wubPlayer");
+    var videoTimer = document.getElementById("wub-video-timer");
     var videoTitle = document.getElementById("VideoTitle");
     var videoDescription = document.getElementById("VideoDescription");
 
     var scaling = wubs.width/640;
-    var videoPlayerHTML =   `<video id="wubPlayer" width="`+wubs.width*scaling+`" height="`+wubs.height*scaling+`" vidID="`+wubs.vidID+`">
+    var videoPlayerHTML =   `<video id="wubPlayer" width="`+wubs.width*scaling+`" height="`+wubs.height*scaling+`" vidID="`+wubs.vidID+`" onclick="Thrimbletrimmer.play()">
                                 <source src="`+wubs.source+`" type="`+wubs.type+`" />
                                 Your browser does not support HTML5 video.
                             </video>`;
 
     videoPlayer.outerHTML = videoPlayerHTML;
+    videoTimer.innerText = getTimeFormat(wubs.startOffset, false);
     videoTitle.value = wubs.title;
     videoDescription.value = wubs.description;
 
-    connectPlayerControls();
-    connectEditorControls();
-}
-
-var connectPlayerControls = function() {
-    //get new elements
-    var $wVideo             = $('#wubPlayer');
-    var $video_container 	= $('.wub-video-player');
-    var $wub_play_btn 		= $('.wub-video-play', $video_container);
-    var $wub_video_seek 	= $('.wub-video-seek', $video_container);
-    var $wub_video_timer 	= $('.wub-video-timer', $video_container);
-    var $wub_volume 		= $('.wub-volume-slider', $video_container);
-    var $wub_volume_btn 	= $('.wub-volume-button', $video_container);
+    ConfigureThrimbletrimmer();
 
     //Create seek bar
-    var seeksliding;			
-    var createSeek = function() {
-        if($wVideo[0].readyState) {
-            var video_duration = $wVideo[0].duration;
-            $wub_video_seek.slider({
-                value: wubs.startOffset,
-                step: 0.01,
-                orientation: "horizontal",
-                range: "min",
-                max: video_duration,
-                animate: false,					
-                slide: function(){							
-                    seeksliding = true;
-                },
-                stop:function(e,ui){
-                    seeksliding = false;						
-                    $wVideo[0].currentTime = ui.value;
-                }
-            });
-            $wVideo[0].currentTime = wubs.startOffset; //Set initial time value for the video.
-            //Add buffer progress bar.
-            var seekBar = $(".wub-video-seek.ui-slider", $video_container).append('<span id="ProgressBar" class="wub-slider-buffered ui-slider-range" ></span>');
-            $wVideo.bind('progress updateMediaState', function () {
-                var buffered = $wVideo[0].buffered;
-                if (!buffered || !buffered.length) {
-                    return;
-                }
-                buffered = getActiveTimeRange(buffered, $wVideo[0].currentTime);
-                $('#ProgressBar', $(seekBar)).width(buffered[2]/$wVideo[0].duration*100 + '%');
-            });
-        } else {
-            setTimeout(createSeek, 150);
-        }
-    };
-    createSeek();
+    Thrimbletrimmer.setSeekBar();
+
+    //Bind time updates
+    Thrimbletrimmer.bindTimeUpdate();
+
+    //Create volume slider
+    Thrimbletrimmer.setVolumeSlider();
     
-    //Hook up the video controls to functions.
-    var wPlay = function() {
-        if($wVideo[0].paused == true) {
-            $wVideo[0].play();				
-        } else {
-            $wVideo[0].pause();
-        }
-    };
-    
-    $wub_play_btn.click(wPlay);
-    $wVideo.click(wPlay);
-    
-    $wVideo.bind('play', function() {
-        $wub_play_btn.addClass('wub-paused-button');
-    });
-    
-    $wVideo.bind('pause', function() {
-        $wub_play_btn.removeClass('wub-paused-button');
-    });
-    
-    $wVideo.bind('ended', function() {
-        $wub_play_btn.removeClass('wub-paused-button');
-    });
-    
-    
-    var gTimeFormat=function(seconds){
-        var date = new Date(null);
-            date.setSeconds(seconds); //Set date based on Seconds.
-            return date.toISOString().substr(11, 8); //Get time segment of date.
-    };
-    
-    var seekUpdate = function() {
-        var currenttime = $wVideo[0].currentTime;
-        if(!seeksliding) $wub_video_seek.slider('value', currenttime);
-        $wub_video_timer.text(gTimeFormat(currenttime));							
-    };
-    
-    $wVideo.bind('timeupdate', seekUpdate);	
-    
-    var video_volume = 1;
-    $wub_volume.slider({
-        value: 1,
-        orientation: "vertical",
-        range: "min",
-        max: 1,
-        step: 0.05,
-        animate: true,
-        slide:function(e,ui){
-                $wVideo[0].muted = false;
-                video_volume = ui.value;
-                $wVideo[0].volume = ui.value;
-            }
-    });
-    
-    var muteVolume = function() {
-        if($wVideo[0].muted) {
-            $wVideo[0].muted = false;
-            $wub_volume.slider('value', video_volume);
-            
-            $wub_volume_btn.removeClass('wub-volume-mute');					
-        } else {
-            $wVideo[0].muted = true;
-            $wub_volume.slider('value', '0');
-            
-            $wub_volume_btn.addClass('wub-volume-mute');
-        };
-    };
-    
-    var getActiveTimeRange = function (range, time) {
-        var len = range.length;
-        var index = -1;
-        var start = 0;
-        var end = 0;
-        for (var i = 0; i < len; i++) {
-            if (time >= (start = range.start(i)) && time <= (end = range.end(i))) {
-                index = i;
-                break;
-            }
-        }
-        return [index, start, end];
-    };
-    //getActiveTimeRange(video.buffered, video.currentTime);
-    
-    $wub_volume_btn.click(muteVolume);
-    
-    $wVideo.removeAttr('controls');
-    
-    //Set initial values.
-    $wub_video_timer.text(gTimeFormat(wubs.startOffset));
+    //Bind play button swap
+    Thrimbletrimmer.bindPlayPause();
+
+    connectEditorControls(); 
 }
 
 var connectEditorControls = function() {
     var video = $('#wubPlayer')[0];
 
     //get new elements
-    var $video_container            = $('.wub-video-player');
-    var $editor_controls 			= $('.wub-video-editorControls', $video_container);
-    var $wub_editor_range 			= $('.wub-editor-selectRange', $editor_controls);
-    var $wub_editor_limitPlayback 	= $('#EndPlaybackCheckbox', $editor_controls);
-    var $wub_editor_start 			= $('#StartTimeStamp', $editor_controls);
-    var $wub_editor_setStartBtn 	= $('#SetStartTimeButton', $editor_controls);
-    var $wub_editor_gotoStartBtn	= $('#GoToStartButton', $editor_controls);
-    var $wub_editor_stop 			= $('#EndTimeStamp', $editor_controls);
-    var $wub_editor_setStopBtn		= $('#SetEndTimeButton', $editor_controls);
-    var $wub_editor_gotoStopBtn		= $('#GoToEndButton', $editor_controls);
+    var $wub_editor_range 			= $('#wub-editor-selectRange');
+    var $wub_editor_limitPlayback 	= $('#EndPlaybackCheckbox');
+    var $wub_editor_start 			= $('#StartTimeStamp');
+    var $wub_editor_setStartBtn 	= $('#SetStartTimeButton');
+    var $wub_editor_gotoStartBtn	= $('#GoToStartButton');
+    var $wub_editor_stop 			= $('#EndTimeStamp');
+    var $wub_editor_setStopBtn		= $('#SetEndTimeButton');
+    var $wub_editor_gotoStopBtn		= $('#GoToEndButton');
     
-    var $wub_editor_Details 		= $('#EditorDetailsPane', $video_container.parent());
-    var $wub_editor_Title 			= $('#VideoTitle', $wub_editor_Details);
-    var $wub_editor_Description 	= $('#VideoDescription', $wub_editor_Details);
-    var $wub_editor_Submit 			= $('#SubmitButton', $wub_editor_Details);
+    var $wub_editor_Title 			= $('#VideoTitle');
+    var $wub_editor_Description 	= $('#VideoDescription');
+    var $wub_editor_Submit 			= $('#SubmitButton');
     
     //Configure Editor Controls
     var createRange = function() {
@@ -297,20 +173,152 @@ var connectEditorControls = function() {
     };
     createRange();
     
-    var getTimeFormat = function(seconds){
-            var date = new Date(null);
-            date.setSeconds(seconds); //Set date based on Seconds.
-            var d = parseInt((seconds % 1)*100); //Get the decimal places to two places for appending.
-            return date.toISOString().substr(11, 8) + "." + d; //Get time segment of date, with two decimal places.
-    };
-    var getSeconds = function(time){
-        var timeArr = time.split(':'), //Array of hours, minutes, and seconds.
-            s = 0, //Seconds total
-            m = 1; //Multiplier
-        while (timeArr.length > 0) { //Iterate through time segments starting from the seconds,
-            s += m * timeArr.pop(); //multiply as  appropriate, and add to seconds total,
-            m *= 60;				//increase multiplier.
-        }
-        return s;
+    
+}
+
+function ConfigureThrimbletrimmer () {
+
+/* Get frequently reused elements */
+Thrimbletrimmer.video           = document.getElementById("wubPlayer");
+
+Thrimbletrimmer.playButton      = document.getElementById('wub-video-play');
+
+Thrimbletrimmer.$seekBar        = $('#wub-video-seek');
+Thrimbletrimmer.videoTimer      = document.getElementById("wub-video-timer");
+
+Thrimbletrimmer.$volumeSlider   = $('#wub-volume-slider');
+Thrimbletrimmer.volumeButton 	= document.getElementById('wub-volume-button');
+
+
+/* Some general reused variables */
+Thrimbletrimmer._seekSliding = false; //Stops the seek bar from being updated while using it to seek.
+Thrimbletrimmer._videoVolume = 1; //Stores the last volume while the video is muted.
+
+/* Configure JQuery Sliders */
+
+//Video Seek Bar
+Thrimbletrimmer.setSeekBar = function () {
+    if(this.video.readyState) {
+        var video_duration = this.video.duration;
+        this.$seekBar.slider({
+            value: wubs.startOffset,
+            step: 0.01,
+            orientation: "horizontal",
+            range: "min",
+            max: video_duration,
+            animate: false,					
+            slide: () => {							
+                this._seekSliding = true;
+            },
+            stop:(e,ui) => {
+                this.video.currentTime = ui.value;
+                this._seekSliding = false;
+            }
+        });
+        this.video.currentTime = wubs.startOffset; //Set initial time value for the video.
+        //Add buffer progress bar.
+        var bufferBar = this.$seekBar.append('<span id="ProgressBar" class="wub-slider-buffered ui-slider-range" ></span>');
+        $('#wubPlayer').bind('progress updateMediaState', () => {
+            var buffered = this.video.buffered;
+            if (!buffered || !buffered.length) {
+                return;
+            }
+            buffered = getActiveTimeRange(buffered, this.video.currentTime);
+            $('#ProgressBar', $(bufferBar)).width(buffered[2]/this.video.duration*100 + '%');
+        });
+    } else {
+        setTimeout(function() { Thrimbletrimmer.setSeekBar() }, 150);
     }
+}
+
+//Volume Slider
+Thrimbletrimmer.setVolumeSlider = function() {
+    this.$volumeSlider.slider({
+        value: 1,
+        orientation: "vertical",
+        range: "min",
+        max: 1,
+        step: 0.05,
+        animate: true,
+        slide:function(e,ui) {
+            this.video.muted = false;
+            this._videoVolume = ui.value;
+            this.video.volume = ui.value;
+        }
+    });
+}
+
+/* Bind Events */
+Thrimbletrimmer.bindPlayPause = function() {
+    this.video.onpause = () => { this.playButton.classList.remove('wub-pause-button'); };
+    this.video.onplay = () => { this.playButton.classList.add('wub-pause-button'); };
+}
+
+Thrimbletrimmer.bindTimeUpdate = function() {
+    this.video.ontimeupdate = () => {
+        var currenttime = this.video.currentTime;
+        if(!this._seekSliding) {
+            this.$seekBar.slider('value', currenttime);
+            this.videoTimer.innerText = getTimeFormat(currenttime, false);
+        }
+    };
+}
+
+/* Button Actions */
+Thrimbletrimmer.play = function() {
+    this.video.paused ? this.video.play():this.video.pause()
+}
+
+Thrimbletrimmer.mute = function() {
+    if(this.video.muted) { //If the video is muted, unmute it.
+        this.video.muted = false;
+        this.$volumeSlider.slider('value', this._videoVolume);
+        this.volumeButton.classList.remove('wub-volume-mute');					
+    } else { //Mute the video
+        this.video.muted = true;
+        this.$volumeSlider.slider('value', '0');
+        this.volumeButton.classList.add('wub-volume-mute');
+    };
+}
+
+
+
+}
+
+
+/* Utility/Time Functions */
+var getActiveTimeRange = function (range, time) {
+    var len = range.length;
+    var index = -1;
+    var start = 0;
+    var end = 0;
+    for (var i = 0; i < len; i++) {
+        if (time >= (start = range.start(i)) && time <= (end = range.end(i))) {
+            index = i;
+            break;
+        }
+    }
+    return [index, start, end];
+};
+//getActiveTimeRange(video.buffered, video.currentTime);
+
+var getTimeFormat = function(seconds, includeDecimals=true){
+    var date = new Date(null);
+    date.setSeconds(seconds); //Set date based on Seconds.
+    var outputString = date.toISOString().substr(11, 8); //Get time segment of date.
+    if(includeDecimals) {
+        var d = parseInt((seconds % 1)*100); //Get the decimal places to two places for appending.
+        outputString += "." + d; //Include fractional seconds in output
+    }
+    return outputString;
+};
+var getSeconds = function(time){
+    var timeArr = time.split(':'), //Array of hours, minutes, and seconds.
+        s = 0, //Seconds total
+        m = 1; //Multiplier
+    while (timeArr.length > 0) { //Iterate through time segments starting from the seconds,
+        s += m * timeArr.pop(); //multiply as  appropriate, and add to seconds total,
+        m *= 60;				//increase multiplier.
+    }
+    return s;
 }
